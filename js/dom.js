6f9aaf3979f15b5ae1c0252fef777c0b39fd43a5
JosephMina23 <josephmesias2000@gmail.com>
2024-10-28 11:24:16 -0500

    Items y colores

--- js/dom.js
+++ js/dom.js
@@ -1,9 +1,15 @@
-const $head = document.querySelector("#head");
+/*const $head = document.querySelector("#head");
 const $body = document.querySelector("body");
-const $footer = document.querySelector(".footer");
+const $footer = document.querySelector(".footer");*/
+
+//$footer.classList.add("color-fuente");
+
+const $header = document.querySelector("#header");
+const $main = document.querySelector("#main");
+const $footer = document.querySelector("#footer");
 const $a = document.querySelector("#a-item");
 const $a1 = document.querySelector("#a-item1");
 
 $a.classList.add("a");
 $a1.classList.add("a1");
-$footer.classList.add("color-fuente");
+$header.classList.add("header");
