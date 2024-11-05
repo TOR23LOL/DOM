f8cfba5a5e325cf33ff68fa9da4b10f1a3e0556b
JosephMina23 <josephmesias2000@gmail.com>
2024-11-05 09:27:02 -0500

    Cambios JavaScript, Index y Estilos

--- js/dom.js
+++ js/dom.js
@@ -13,3 +13,51 @@ const $a1 = document.querySelector("#a-item1");
 $a.classList.add("a");
 $a1.classList.add("a1");
 $header.classList.add("header");
+
+const $repositorio = document.querySelector("#repositorio");
+const $parrafo = document.querySelector(".p");
+
+const $section = document.querySelector(".flex-section");
+
+$parrafo.setAttribute("id", "p2");
+console.log($repositorio.getAttribute("href"));
+
+const $agregarParrafo = document.querySelector(".addP");
+
+function addParrafo() { 
+    let parrafo2 = document.createElement("p");
+    let infoP = document.createTextNode("PARRAFO PARRAFO");
+    parrafo2.appendChild(infoP);
+    parrafo2.setAttribute("class", "parrafo2");
+    $section.appendChild(parrafo2);
+}
+
+$agregarParrafo.addEventListener("click", addParrafo);
+
+const $section2 = document.querySelector(".flex-section1");
+
+//let dias = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo']
+
+//let ul = document.createElement("ul");
+
+//$section2.appendChild(ul);
+
+/*dias.forEach(item =>{
+    let li = document.createElement("li");
+    li.textContent = item;
+    ul.appendChild(li);
+})*/
+
+let meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+
+let ul = document.createElement("ul");
+$section2.appendChild(ul);
+let fragmento = document.createDocumentFragment();
+
+meses.forEach(item =>{
+    let li = document.createElement("li");
+    li.textContent = item;
+    fragmento.appendChild(li);
+})
+
+ul.appendChild(fragmento);
